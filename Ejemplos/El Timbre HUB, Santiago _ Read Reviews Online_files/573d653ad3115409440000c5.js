bd27bc784aabc5634e03b5bb75a35fd73813906a
Juan Muñoz <alexx44@users.noreply.github.com>
2016-08-10 13:43:51 -0400

    Ejemplos

---
+++ Ejemplos/El Timbre HUB, Santiago _ Read Reviews Online_files/573d653ad3115409440000c5.js
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>
+      Relsa Chile - Página Bloqueada
+    </title>
+    <style type="text/css">
+      h3 {
+        color: #F00;
+        font-family: Tahoma, Geneva, sans-serif;
+      }
+      td {
+        font-family: Tahoma, Geneva, sans-serif;
+        font-size: 14px;
+      }
+    </style>
+  </head>
+ 
+<img src="http://www.svg.cl/Clientes/images/relsa.jpg" width="209" height="86" alt="" />
+
+<body bgcolor=white lang=ES link=blue vlink=purple style='tab-interval:35.4pt'>
+
+<div class=WordSection1>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'>Estimado(a)
+usuario(a).<o:p></o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'><o:p>&nbsp;</o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'>Este sitio
+web ha sido bloqueado por políticas de la compañía.<o:p></o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'>Si por
+razones propias del trabajo que realiza a diario requiere acceso a este sitio
+web, por favor comuníquese con la Mesa de Ayuda (<a
+href="mailto:soporte@relsa.cl?subject=Consulta%20bloqueo%20web">soporte@relsa.cl</a>).<o:p></o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'><o:p>&nbsp;</o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif"'>Atentamente.<o:p></o:p></span></p>
+
+<p class=MsoNoSpacing><span style='font-family:"Arial","sans-serif";mso-fareast-font-family:
+"Times New Roman"'>Sub-gerencia Informática.<o:p></o:p></span></p>
+
+</div>
+
+</body>
+  
+</html>
